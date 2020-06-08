ccf75db6ec69dac2dc8aee725d4c3212d330fade
KonstantinosLaloudakis <66478154+KonstantinosLaloudakis@users.noreply.github.com>
2020-06-08 18:49:06 +0300

    v1.2
    Comments added and bug fixes

--- func.js
+++ func.js
@@ -9,7 +9,7 @@ var boundaryX1 = 0;
 var boundaryX2 = 100;
 var boundaryY1 = 0;
 var boundaryY2 = 70;
-//window.addEventListener("load", () => {
+
 const museum = document.querySelector("#museum");
 
 var svgNS = "http://www.w3.org/2000/svg"; 
@@ -22,7 +22,7 @@ const museumPoint = (elem, x, y) => {
   return p.matrixTransform(elem.getScreenCTM().inverse());
 }
 
-
+//onclick function of the 1st button
 document.getElementById("btn1").addEventListener("click",function(){
 	
 	var myImage = document.createElementNS(svgNS,"image");	//to create a circle. for rectangle use "rectangle"
@@ -31,18 +31,19 @@ document.getElementById("btn1").addEventListener("click",function(){
     myImage.setAttributeNS(null,"width","10%");
     myImage.setAttributeNS(xlink,"href","sparta.png");
 	myImage.setAttribute("class","draggable confine");
-	console.log(myImage);
 	document.getElementById("museum").appendChild(myImage);
 	
 
 });
+
+//onclick function of the 2nd button
 document.getElementById("btn2").addEventListener("click", createRect);
 
 
 	
 	function createRect(){
 		button2Clicked=true;
-	
+	//on mousedown start creating the rectangle
 	museum.addEventListener("mousedown",(event) =>{
 		if(button2Clicked){
 			button2Clicked=false;
@@ -72,7 +73,7 @@ document.getElementById("btn2").addEventListener("click", createRect);
 			
 			
 			}
-		
+			//stop drawing when the mouse is up
 			const endDrawRect = (e) => {
 				museum.removeEventListener('mousemove', drawRect);
 				museum.removeEventListener('mouseup', endDrawRect);
@@ -86,26 +87,25 @@ document.getElementById("btn2").addEventListener("click", createRect);
 	
 }
 
-
+//onclick function of the 3rd button
 document.getElementById("btn3").addEventListener("click", function(){
 	var myPolyline = document.createElementNS(svgNS,"polyline"); 
-    //myPolyline.setAttributeNS(null,"id","mypolyline");
     myPolyline.setAttributeNS(null,"points","0,0 0,60 90,60 90,0 0,0");
     myPolyline.setAttributeNS(null,"width",5);
     myPolyline.setAttributeNS(null,"height",5);
     myPolyline.setAttributeNS(null,"fill","transparent");
     myPolyline.setAttributeNS(null,"stroke","black");
 	myPolyline.setAttribute("class","draggable confine");
-	console.log(myPolyline);
-    museum.appendChild(myPolyline);
+	museum.appendChild(myPolyline);
 });
 
+//onclick function of the 4th button
 document.getElementById("btn4").addEventListener("click", createLine);
 
 function createLine(){
 		button4Clicked=true;
 	
-	
+	//on mousedown start creating the line
 	museum.addEventListener("mousedown",(event) =>{
 	
 		if(button4Clicked){
@@ -115,7 +115,6 @@ function createLine(){
 			const drawLine= (e) =>{
 				
 				let p= museumPoint(museum,e.clientX,e.clientY);
-				//line.setAttributeNS(null,'id','line');
 				line.setAttributeNS(null, 'x1', start.x);
 				line.setAttributeNS(null, 'y1', start.y);
 				line.setAttributeNS(null, 'x2', p.x);
@@ -125,7 +124,7 @@ function createLine(){
 				document.getElementById("museum").appendChild(line);
 			
 			}
-			
+		//stop drawing when the mouse is up
 		const endDrawLine = (e) => {
 			museum.removeEventListener('mousemove', drawLine);
 			museum.removeEventListener('mouseup', endDrawLine);
@@ -139,8 +138,82 @@ function createLine(){
 
 
 }
+//remove an object of your choice
+document.getElementById("btn5").addEventListener("click",removeObject);
+
+function removeObject(){
+	button5Clicked=true;
+	museum.addEventListener("mousedown",(event) =>{
+		if(button5Clicked){
+			button5Clicked=false;
+			selectedElement=event.target;
+			if(selectedElement!=document.getElementById("museum")){
+				var object = selectedElement;
+				var parent = object.parentNode;
+				parent.removeChild(object);
+			}
+			else{
+				alert("Ρε την παλεύεις; Πας να διαγράψεις κατι που δεν υπάρχει!");
+			}
+		}
+	});
+}
+
+document.getElementById("btn6").addEventListener("click",save);
+
+//save all the current objects
+function save(){
+	
+	//delete all previous saved objects
+	while (objects.length) { objects.pop(); }
+	
+	if(document.getElementById('museum').childNodes.length>1){
+		for(var i in document.getElementById('museum').childNodes){
+			objects.push(document.getElementById('museum').childNodes[i]);
+		}
+	}
+	//if the viewbox is empty nothing happens
+	else{
+		alert("There is nothing to save");
+	}
+}
+
+document.getElementById("btn7").addEventListener("click",load);
+
+//load all previous saved objects 
+function load(){
+	if(objects.length){
+		console.log(objects);
+	for(var i in objects)
+		document.getElementById('museum').appendChild(objects[i]);
+	
+	}
+	
+	//if the objects array is empty, it means that the save function hasn't been called yet, so we inform the user
+	else{
+		alert("There isn't a saved session");
+	}
+	
+}
 
 
+//onclick function of the 8th button
+document.getElementById("btn8").addEventListener("click",function(){
+	
+	var myImage = document.createElementNS(svgNS,"image");
+	myImage.setAttributeNS(null,"height","10%");
+    myImage.setAttributeNS(null,"width","10%");
+    myImage.setAttributeNS(xlink,"href","door.png");
+	myImage.setAttribute("class","draggable confine");
+	document.getElementById("museum").appendChild(myImage);
+	
+
+});
+
+//------------------------------------------------
+
+
+//makes all objects inside the viebox draggable
 function makeDraggable(evt) {
   var svg = evt.target;
   svg.addEventListener('mousedown', startDrag);
@@ -148,59 +221,55 @@ function makeDraggable(evt) {
   svg.addEventListener('mouseup', endDrag);
   svg.addEventListener('mouseleave', endDrag);
   function startDrag(evt) {
-	  if(!button2Clicked && !button4Clicked){
+	  //if there is a button pressed don't drag the object
+	if(!button2Clicked && !button4Clicked){
 		  
-		  confined = evt.target.classList.contains('confine');
-		  if (confined) {
-			  bbox = evt.target.getBBox();
-			  minX = boundaryX1 - bbox.x;
-			  maxX = boundaryX2 - bbox.x - bbox.width;
-			  minY = boundaryY1 - bbox.y;
-			  maxY = boundaryY2 - bbox.y - bbox.height;
-		  }
-  	if (evt.target.classList.contains('draggable')) {
-		selectedElement = evt.target;
-		console.log(evt.target);
-	    offset = getMousePosition(evt);
+		//constrain the movement of the objects inside the viewbox
+		confined = evt.target.classList.contains('confine');
+		if (confined) {
+			bbox = evt.target.getBBox();
+			minX = boundaryX1 - bbox.x;
+			maxX = boundaryX2 - bbox.x - bbox.width;
+			minY = boundaryY1 - bbox.y;
+			maxY = boundaryY2 - bbox.y - bbox.height;
+		}
+		
+		if (evt.target.classList.contains('draggable')) {
+			selectedElement = evt.target;
+			offset = getMousePosition(evt);
    
-       var transforms = selectedElement.transform.baseVal;
-    // Ensure the first transform is a translate transform
-    if (transforms.length === 0 ||
-        transforms.getItem(0).type !== SVGTransform.SVG_TRANSFORM_TRANSLATE) {
-      // Create an transform that translates by (0, 0)
-      var translate = svg.createSVGTransform();
-      translate.setTranslate(0, 0);
-      // Add the translation to the front of the transforms list
-      selectedElement.transform.baseVal.insertItemBefore(translate, 0);
-    }
-    // Get initial translation amount
-    transform = transforms.getItem(0);
-	offset.x -= transform.matrix.e;
-    offset.y -= transform.matrix.f;
+			var transforms = selectedElement.transform.baseVal;
+			// Ensure the first transform is a translate transform
+			if (transforms.length === 0 || transforms.getItem(0).type !== SVGTransform.SVG_TRANSFORM_TRANSLATE) {
+				// Create an transform that translates by (0, 0)
+				var translate = svg.createSVGTransform();
+				translate.setTranslate(0, 0);
+				// Add the translation to the front of the transforms list
+				selectedElement.transform.baseVal.insertItemBefore(translate, 0);
+			}
+			// Get initial translation amount
+			transform = transforms.getItem(0);
+			offset.x -= transform.matrix.e;
+			offset.y -= transform.matrix.f;
+		}
 	}
   }
-  }
+  
   function drag(evt) {
   	if (selectedElement) {
 		evt.preventDefault();
-           var coord = getMousePosition(evt);
-		   var dx=coord.x - offset.x;
-		   var dy=coord.y - offset.y;
-		
-		   if (confined) {
-			  if (dx < minX) { dx = minX; }
-			  else if (dx > maxX) { dx = maxX; }
-			  if (dy < minY) { dy = minY; }
-			  else if (dy > maxY) { dy = maxY; }
-			}
-
-
+		var coord = getMousePosition(evt);
+		var dx=coord.x - offset.x;
+		var dy=coord.y - offset.y;
+	
+		if (confined) {
+			if (dx < minX) { dx = minX; }
+			else if (dx > maxX) { dx = maxX; }
+			if (dy < minY) { dy = minY; }
+			else if (dy > maxY) { dy = maxY; }
+		}
 
         transform.setTranslate(dx, dy);
-		for(var i in document.getElementById("museum").childNodes){
-			if(selectedElement==document.getElementById("museum").childNodes[i])
-				document.getElementById("museum").childNodes[i].x+=coord.x - offset.x;
-		}
 		
 	}
   }
@@ -217,59 +286,19 @@ function makeDraggable(evt) {
       };
     }
 }
-document.getElementById("btn5").addEventListener("click",removeObject);
 
-function removeObject(){
-	button5Clicked=true;
-	museum.addEventListener("mousedown",(event) =>{
-		if(button5Clicked){
-			button5Clicked=false;
-			selectedElement=event.target;
-			if(selectedElement!=document.getElementById("museum")){
-				var object = selectedElement;
-				var parent    = object.parentNode;
-				parent.removeChild(object);
-			}
-			else{
-				alert("Ρε την παλεύεις; Πας να διαγράψεις κατι που δεν υπάρχει!");
-			}
-		}
-	});
-}
+/*function show_svg()
+{
+  var svg = document.getElementById("museum");
+  var serializer = new XMLSerializer();
+  var ser = serializer.serializeToString(svg);
+  var w = window.open();
+  w.document.open();
+  w.document.write(ser);
+  w.document.close();
+}*/
 
-document.getElementById("btn6").addEventListener("click",save);
 
-function save(){
-	
-	while (objects.length) { objects.pop(); }
-	for(var i in document.getElementById('museum').childNodes)
-		if(i)
-		objects.push(document.getElementById('museum').childNodes[i]);
-	console.log(objects);
-	
-}
 
-document.getElementById("btn7").addEventListener("click",load);
 
-function load(){
-	for(var i in objects)
-		document.getElementById('museum').appendChild(objects[i]);
-	
-	
-	
-}
-
-document.getElementById("btn8").addEventListener("click",function(){
-	
-	var myImage = document.createElementNS(svgNS,"image");	//to create a circle. for rectangle use "rectangle"
-	//myImage.setAttributeNS(null,"id","myimage");
-    myImage.setAttributeNS(null,"height","10%");
-    myImage.setAttributeNS(null,"width","10%");
-    myImage.setAttributeNS(xlink,"href","door.png");
-	myImage.setAttribute("class","draggable confine");
-	console.log(myImage);
-	document.getElementById("museum").appendChild(myImage);
-	
-
-});
 
